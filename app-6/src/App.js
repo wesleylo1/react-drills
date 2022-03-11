5181ae55495a8af9a3a46414403e8d43ac4133db
Wesley Lo <wesleylo1995@gmail.com>
2022-03-11 17:54:05 -0600

    app 6 solved

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Create an app that displays a to-do list. You will need two components, the App component and a Todo component. The App component should be responsible for getting new tasks and storing the list of tasks. The Todo component should be responsible for displaying the individual tasks from the App component. The App component should create a list of 'Todo' components passing down a task into the Todo component as a prop and display the list.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react"
+import "./App.css"
+import Todo from "./Todo"
+
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            input: "",
+            list: []
+        }
+
+        this.handleAddTask = this.handleAddTask.bind(this)
+    }
+
+    handleInputChange(value) {
+        this.setState({ input: value })
+    }
+
+    handleAddTask() {
+        this.setState({
+            list: [...this.state.list, this.state.input],
+            input: ""
+        })
+        console.log(this.state.list)
+    }
+
+    render() {
+        let list = this.state.list.map((element, index) => {
+            return <Todo key={index} task={element} />
+        })
+
+        return (
+            <div className="App">
+                <h1>My to-do list:</h1>
+
+                <div>
+                    <input
+                        value={this.state.input}
+                        placeholder="Enter new task"
+                        onChange={(e) => this.handleInputChange(e.target.value)}
+                    />
+
+                    <button onClick={this.handleAddTask}>Add</button>
+                </div>
+
+                <br />
+
+                {list}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
