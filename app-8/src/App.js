c8b73713b930d47b045ad5ee7f32cb16ccd0ce24
Wesley Lo <wesleylo1995@gmail.com>
2022-03-12 10:08:31 -0600

    finished app-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Create an app hitting an API of your choice (swapi.co, birdapi.com, pokeapi, smurfs, marvel api, etc). The API should be hit as soon as the component is finished rendering. The app should set value(s) on state based on results from the API and then show the propertie(s) on state in the DOM.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// The axios package should be used to hit an API.
+
+import React, { Component } from "react"
+import "./App.css"
+import axios from "axios"
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      lukeSkywalker: ""
+    }
+  }
+
+  mountComponent() {
+    axios
+      .get("https://swapi.dev/api/people/1")
+      .then((res) => {
+        this.setState({
+          lukeSkywalker: res.data
+        })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  render() {
+    this.mountComponent()
+    return (
+      <div className="App">
+        <h1>Name: {this.state.lukeSkywalker.name} </h1>
+        <h1>Birth Year: {this.state.lukeSkywalker.birth_year} </h1>
+        <h1>Height: {this.state.lukeSkywalker.height} </h1>
+        <h1>Eye Color: {this.state.lukeSkywalker.eye_color} </h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
